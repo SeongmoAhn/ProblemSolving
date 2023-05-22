cb1752c937ababbc0ccdb792b5b0a0d83b43087b
SeongmoAhn <asm0619@naver.com>
2023-05-22 15:29:21 +0900

    이코테 3-2 주석

--- TCT3-2.cpp
+++ TCT3-2.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// M의 값이 커졌을 때의 알고리즘
 int main()
 {
     int n, m, k;
@@ -17,14 +18,15 @@ int main()
         v.push_back(input);
     }
     sort(v.begin(), v.end());
-    first = v[n - 1];
-    second = v[n - 2];
+    first = v[n - 1]; // 입력 값 중 가장 큰 값
+    second = v[n - 2]; // 입력 값 중 두 번째로 큰 값
 
+    // 가장 큰 값이 쓰여지는 횟수
     count = int(m / (k + 1)) * k;
     count += m % (k + 1);
 
     result += (count) * first;
-    result += (m - count) * second;
+    result += (m - count) * second; // (m - count)는 두번 째로 큰 값이 쓰여지는 횟수
 
     cout << result << endl;
 }
