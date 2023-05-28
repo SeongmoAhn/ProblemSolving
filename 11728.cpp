c183e1b1a5cc00f545cae8e136307faf48e9472b
SeongmoAhn <asm0619@naver.com>
2023-05-28 20:15:51 +0900

    baekjoon 11728

---
+++ 11728.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    cout.tie(NULL);
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int input;
+    int n, m, i;
+    vector<int> v1, v2;
+
+    cin >> n >> m;
+    for (i = 0; i < n; i++) {
+        cin >> input;
+        v1.push_back(input);
+    }
+    for (i = 0; i < m; i++) {
+        cin >> input;
+        v2.push_back(input);
+    }
+    for (i = 0; i < m; i++) {
+        v1.push_back(v2[i]);
+    }
+
+    sort(v1.begin(), v1.end());
+    for (i = 0; i < v1.size(); i++) {
+        cout << v1[i] << " ";
+    }
+    cout << '\n';
+
+	return 0;
+}
