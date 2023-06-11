ef5e5d64e9828ed317808c71a88eecb5006f40d1
SeongmoAhn <asm0619@naver.com>
2023-06-11 22:40:52 +0900

    baekjoon 15652

---
+++ 15652.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[10];
+
+void rec(int x, int len) {
+    if (len == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = x; i <= n; i++) {
+        arr[len] = i;
+        rec(i, len + 1);
+    }
+}
+
+int main(void)
+{
+    cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
+    cin >> n >> m;
+
+    rec(1, 0);
+
+    return 0;
+}
