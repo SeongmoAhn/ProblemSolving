5a53cfcf772e854bf422212c7c6060411993647d
SeongmoAhn <asm0619@naver.com>
2023-06-26 16:25:46 +0900

    baekjoon 1197 kruskal

---
+++ 1197kruskal.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <tuple>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#define PS_INPUT cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false)
+#define endl '\n'
+#define INF 1e9
+#define pii pair<int, int>
+#define ll long long
+#define SIZE 10001
+
+int V, E;
+ll ans;
+int parent[SIZE];
+vector<tuple<int, int, int>> graph;
+
+void initInput() {
+    cin >> V >> E;
+    for (int i = 1; i <= V; i++) parent[i] = i;
+
+    int a, b, c;
+    for (int i = 0; i < E; i++) {
+        cin >> a >> b >> c;
+        graph.push_back({c, a, b});
+    }
+    sort(graph.begin(), graph.end());
+}
+
+int getParent(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = getParent(parent[x]);
+}
+
+bool unionParent(int v1, int v2) {
+    int u = getParent(v1);
+    int v = getParent(v2);
+    if (u == v) return false;
+    else {
+        parent[u] = v;
+        return true;
+    }
+}
+
+void kruskal() {
+    for (int i = 0; i < E; i++) {
+        if (unionParent(get<1>(graph[i]), get<2>(graph[i])))
+            ans += get<0>(graph[i]);
+    }
+}
+
+void solve() {
+    kruskal();
+    cout << ans << endl;
+}
+
+int main(void)
+{
+    PS_INPUT; 
+    initInput();
+    solve();
+
+    return 0;
+}
