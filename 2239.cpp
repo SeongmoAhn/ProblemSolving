ca9c36c55de7cf23c9d847783171bed3ae42c0e4
SeongmoAhn <asm0619@naver.com>
2023-06-28 20:40:52 +0900

    baekjoon 2239

---
+++ 2239.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define PS_INPUT cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false)
+#define endl '\n'
+#define INF 1e9
+#define pii pair<int, int>
+#define ll long long
+#define SIZE 9
+
+int graph[SIZE][SIZE];
+bool isInRow[SIZE][SIZE];
+bool isInCol[SIZE][SIZE];
+bool isInSquare[SIZE][SIZE];
+
+int getSquare(int y, int x) {
+    return (y / 3) * 3 + (x / 3);
+}
+
+void initInput() {
+    string input;
+    for (int row = 0; row < SIZE; row++) {
+        cin >> input;
+        for (int col = 0; col < SIZE; col++) {
+            graph[row][col] = input[col] - '0';
+
+            if (graph[row][col]) {
+                isInRow[row][graph[row][col]] = true; 
+                isInCol[col][graph[row][col]] = true; 
+                isInSquare[getSquare(row, col)][graph[row][col]] = true;
+            }
+        }
+    }
+}
+
+void printGraph() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << graph[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void DFS(int cnt) {
+    int row = cnt / SIZE;
+    int col = cnt % SIZE;
+    if (cnt == 81) {
+        printGraph();
+        exit(0);
+    }
+
+    if (!graph[row][col]) {
+        for (int i = 1; i <= 9; i++) {
+            if ((!isInRow[row][i]) && (!isInCol[col][i]) && (!isInSquare[getSquare(row, col)][i])) {
+                isInRow[row][i] = true;
+                isInCol[col][i] = true;
+                isInSquare[getSquare(row, col)][i] = true;
+                graph[row][col] = i;
+                DFS(cnt + 1);
+                graph[row][col] = 0;
+                isInRow[row][i] = false;
+                isInCol[col][i] = false;
+                isInSquare[getSquare(row, col)][i] = false;
+            }
+        }
+    }
+    else DFS(cnt + 1);
+}
+
+void solve() {
+    DFS(0);
+}
+
+int main(void)
+{
+    PS_INPUT; 
+    initInput();
+    solve();
+
+    return 0;
+}
