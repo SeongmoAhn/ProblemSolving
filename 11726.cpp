8f0d99c2fa4f1dce29926c87831654507f01f153
SeongmoAhn <asm0619@naver.com>
2023-07-01 14:08:26 +0900

    baekjoon 11726

---
+++ 11726.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define SIZE 1001
+
+int n;
+int dp[SIZE];
+
+void initInput() {
+    cin >> n;
+}
+
+void solve() {
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+    cout << dp[n] << endl;
+}
+
+int main(void)
+{
+    cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
+    initInput();
+    solve();
+
+    return 0;
+}
