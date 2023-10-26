bccca308ef7b9bac26e4a82c6d7af5bccd1850fc
SeongmoAhn <asm0619@naver.com>
2023-10-26 11:57:15 +0900

    baekjoon 15988

---
+++ 15988.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define SIZE 1000001
+#define MOD 1000000009
+
+void solve() {
+    int T; cin >> T;
+    ll dp[SIZE][3] = {{0, 0, 0}, {1, 0, 0}, {1, 1, 0}, {2, 1, 1}, };
+    vector<int> N;
+    int M = -1;
+    for (int i = 0; i < T; i++) {
+        int input; cin >> input;
+        N.push_back(input);
+        M = max(M, input);
+    }
+
+    for (int i = 4; i <= M; i++) {
+        dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+        dp[i][1] = (dp[i - 2][0] + dp[i - 2][1] + dp[i - 2][2]) % MOD;
+        dp[i][2] = (dp[i - 3][0] + dp[i - 3][1] + dp[i - 3][2]) % MOD;
+    }
+
+    for (int i = 0; i < T; i++) {
+        int cur = N[i];
+        cout << (dp[cur][0] + dp[cur][1] + dp[cur][2]) % MOD << endl;
+    }
+}
+
+int main(void) {
+    cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
+    // freopen("input.txt", "r", stdin);
+    solve();
+
+    return 0;
+}
