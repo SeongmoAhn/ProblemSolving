842b97ee6d324bf95b7f5865b8d3601199282b08
SeongmoAhn <asm0619@naver.com>
2023-11-06 21:55:42 +0900

    baekjoon 11722

---
+++ 11722.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define endl '\n'
+#define SIZE 1001
+
+int n, ans;
+int dp[SIZE];
+int input[SIZE];
+
+void initInput() {
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> input[i];
+}
+
+void solve() {
+    for (int i = 1; i <= n; i++) {
+        dp[i] = 1;
+        for (int j = i - 1; j >= 1; j--) {
+            if (input[i] < input[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        ans = max(ans, dp[i]);
+    }
+    cout << ans << endl;
+}
+
+int main(void)
+{
+    cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
+    // freopen("input.txt", "r", stdin);
+    initInput();
+    solve();
+
+    return 0;
+}
