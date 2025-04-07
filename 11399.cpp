f4fde6198dd8b0bbcc85f38d561a80b1ab92bc91
SeongmoAhn <asm0619@naver.com>
2025-04-07 20:58:57 +0900

    baekjoon 11399.cpp

--- 11399.cpp
+++ 11399.cpp
@@ -2,30 +2,39 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+#define endl '\n'
+
+vector<int> v;
+
+void init_input()
+{
+    int N, n;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> n;
+        v.push_back(n);
+    }
+}
+
+int solve()
+{
+    int ans = 0;
+    int temp = 0;
+    sort(v.begin(), v.end());
+    for (auto cur : v) {
+        temp += cur;
+        ans += temp;
+    }
+
+    return ans;
+}
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-    int n;
-    int input;
-    int sum = 0;
-    int i, j;
-    vector<int> v;
-    cin >> n;
+    cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
+    // freopen("input.txt", "r", stdin);
+    init_input();
+    cout << solve() << endl;
 
-    for (i = 0; i < n; i++) {
-        cin >> input;
-        v.push_back(input);
-    }
-
-    sort(v.begin(), v.end());
-
-    for (i = 0, j = n; i < n; i++, j--) {
-        sum += v[i] * j;
-    }
-
-    cout << sum << '\n';
-
-	return 0;
+    return 0;
 }
