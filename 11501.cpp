094cf3f3c09b60fcbbeaf86612eeaa58ce2d6ea7
SeongmoAhn <asm0619@naver.com>
2025-04-07 14:31:43 +0900

    baekjoon 11501.cpp

--- 11501.cpp
+++ 11501.cpp
@@ -3,22 +3,41 @@
 #include <vector>
 using namespace std;
 #define endl '\n'
-#define SIZE 
+#define ll long long
 
-void init_input()
+ll solve()
 {
-}
+    int N; cin >> N;
+    vector<int> v;
+    int n;
+    while (N--) {
+        cin >> n;
+        v.push_back(n);
+    }
 
-void solve()
-{
+    reverse(v.begin(), v.end());
+    ll ans = 0;
+    int pivot = -1;
+    for (auto n : v) {
+        if (n > pivot) {
+            pivot = n;
+            continue;
+        }
+
+        ans += (pivot - n);
+    }
+
+    return ans;
 }
 
 int main(void)
 {
     cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
-    freopen("input.txt", "r", stdin);
-    init_input();
-    solve();
+    // freopen("input.txt", "r", stdin);
+    int T; cin >> T;
+    while (T--) {
+        cout << solve() << endl;
+    }
 
     return 0;
 }
