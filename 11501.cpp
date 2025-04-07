467e2aa6e09ac01d79d693a3924a8e7cfb960553
SeongmoAhn <asm0619@naver.com>
2025-04-07 12:51:51 +0900

    baekjoon 11501.cpp

--- 11501.cpp
+++ 11501.cpp
@@ -1,38 +1,24 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
+#define endl '\n'
+#define SIZE 
+
+void init_input()
+{
+}
+
+void solve()
+{
+}
 
 int main(void)
 {
-    cout.tie(NULL);
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    int testCase, n, input, i, j, k, size;
-    long long sum = 0;
-    int maxValue;
-    cin >> testCase;
-    vector<int> v;
+    cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
+    freopen("input.txt", "r", stdin);
+    init_input();
+    solve();
 
-    for (i = 0; i < testCase; i++) {
-        cin >> n;
-        v.clear();
-        maxValue = -1;
-        sum = 0;
-        for (j = 0; j < n; j++) {
-            cin >> input;
-            v.push_back(input);
-        }
-
-        for (j = n - 1; j >= 0; j--) {
-            if (maxValue < v[j]) {
-                maxValue = v[j];
-            }
-            else {
-                sum += maxValue - v[j];
-            }
-        }
-        cout << sum << '\n';
-    }
-
-	return 0;
+    return 0;
 }
