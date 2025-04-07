4a7ae0206421d5fe33ec99b86fd8f7b9afbafcb8
SeongmoAhn <asm0619@naver.com>
2025-04-07 14:56:51 +0900

    baekjoon 11047.cpp

--- 11047.cpp
+++ 11047.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <functional>
 using namespace std;
+#define endl '\n'
+
+int N, K;
+vector<int> v;
+
+void init_input()
+{
+    cin >> N >> K;
+    int n;
+    for (int i = 0; i < N; i++) {
+        cin >> n; v.push_back(n);
+    }
+}
+
+int solve()
+{
+    reverse(v.begin(), v.end());
+
+    int ans = 0;
+    for (auto coin : v) {
+        ans += (K / coin);
+        K %= coin;
+    }
+
+    return ans;
+}
 
 int main(void)
 {
     cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
-    int input, n, m, i, j;
-    vector<int> coins;
-    int money;
-    cin >> n >> money;
-    for (i = 0; i < n; i++) {
-        cin >> input;
-        coins.push_back(input);
-    }
-    sort(coins.begin(), coins.end(), greater<>());
-
-    int cnt = 0;
-    i = 0;
-    while (money != 0) {
-        while (money >= coins[i]) {
-            cnt++;
-            money -= coins[i];
-        }
-        i++;
-    }
-    cout << cnt << '\n';
+    // freopen("input.txt", "r", stdin);
+    init_input();
+    cout << solve() << endl;
 
     return 0;
 }
